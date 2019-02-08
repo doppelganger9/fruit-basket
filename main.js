eae310a437ce3251d0adfcd7c1a94937c38e03b2
David Lacourt <david.lacourt@gmail.com>
2019-02-08 19:29:02 +0100

    Adds npm start run script + simple main implementation

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-const totalPrice = require('./total-price');
+#!/usr/bin/env node
+const totalPrice = require('./lib/total-price');
 
-// TODO CLI...
+console.log(JSON.stringify(totalPrice({
+    orderedItemsByType: {
+      'Apple': 4,
+      'Orange': 3,
+      'Watermelon': 5,
+    }
+}), null, 2));
