2f155acd29ac21d76788f5bfd430deb6fb1113ae
David LACOURT <david.lacourt@gmail.com>
2019-02-10 10:53:14 +0100

    Adds CI test to enforce Mutation Testing thresholds

--- stryker.conf.js
+++ stryker.conf.js
@@ -2,8 +2,9 @@ module.exports = function(config) {
   config.set({
     mutator: "javascript",
     packageManager: "npm",
-    reporters: ["clear-text", "progress", "html", "dashboard" ],
+    reporters: ["clear-text", "progress", "html", "dashboard"],
     testRunner: "jest",
-    coverageAnalysis: "off"
+    coverageAnalysis: "off",
+    thresholds: { high: 90, low: 70, break: 50 }
   });
 };
