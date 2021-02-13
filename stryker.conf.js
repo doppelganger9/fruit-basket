c61c3531b67d33984877961616405ec330413f1a
David LACOURT <david.lacourt+github@gmail.com>
2021-02-13 21:32:23 +0100

    update to latest packages; stryker v4 migration breaking changes

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,10 +1,10 @@
-module.exports = function(config) {
-  config.set({
-    mutator: "javascript",
-    packageManager: "npm",
-    reporters: ["clear-text", "progress", "html", "dashboard"],
-    testRunner: "jest",
-    coverageAnalysis: "off",
-    thresholds: { high: 90, low: 70, break: 50 }
-  });
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+  packageManager: "npm",
+  reporters: ["clear-text", "progress", "html", "dashboard"],
+  testRunner: "jest",
+  coverageAnalysis: "off",
+  thresholds: { high: 90, low: 70, break: 50 }
 };
